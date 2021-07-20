7376183cb7c02750a6c8282e80cbfa79efb90677
Paul Fremantle <pzfreo@github.com>
2021-07-20 19:47:27 +0100

    .

--- main.tf
+++ main.tf
@@ -1,17 +1,22 @@
 # main.tf
+
+# Set your student id / AWS name here. It will be appended to all the resources that are createdo 
+variable "student" {
+  type = string
+  default = "pzf2"
+}
+
+# Other properties can be set here
 variable "awsprops" {
     type = map(string)
     default = {
     region = "eu-west-1"
     instance_type = "t3.micro"
-    keyname = "pzf2"
+    dc-repository = "https://github.com/pzfreo/clo-tf-docker.git"
   }
 }
-variable "student" {
-  type = string
-  default = "oxclo01"
-}
 
+# set the AWS region to use 
 provider "aws" {
   region = lookup(var.awsprops, "region")
 }
@@ -20,16 +25,20 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
+# Use AWS VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-
+# Use the default AWS subnet
 data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
 ### EC2
+
+# create a security group to allow SSH
+# Ideally this would be modified to only allow specific IP addresses
 
 module "dev_ssh_sg" {
   source = "terraform-aws-modules/security-group/aws"
@@ -38,9 +47,15 @@ module "dev_ssh_sg" {
   description = format("Security group for dev_ssh_sg %s", var.student)
   vpc_id      = data.aws_vpc.default.id
 
+# You can change this to your public IP if needed
+# ingress_cidr_blocks = ["214.83.74.111/32"]
+
+# Allow SSH from anywhere IPv4
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["ssh-tcp"]
 }
+
+# Create a security group to enable web access to the server
 
 module "ec2_sg" {
   source = "terraform-aws-modules/security-group/aws"
@@ -54,6 +69,8 @@ module "ec2_sg" {
   egress_rules        = ["all-all"]
 }
 
+
+# Use the most recent Ubuntu VM
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -71,6 +88,8 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+
+# create a IAM role to allow the EC2 instance permission to act on your behalf
 
 resource "aws_iam_role" "ec2_role_clo_tfdc" {
   name        = format("ec2_role_clo_tfdc_%s",var.student)
@@ -91,25 +110,28 @@ resource "aws_iam_role" "ec2_role_clo_tfdc" {
 }
 EOF
 
- 
 }
 
+# Create an instance profile using the IAM role
 resource "aws_iam_instance_profile" "ec2_profile_clo_tfdc" {
   name = format("ec2_role_clo_tfdc_%s",var.student) 
   role = aws_iam_role.ec2_role_clo_tfdc.name
 }
 
 
+# Create an AWS EC2 Instance
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = lookup(var.awsprops, "instance_type")
 
-  key_name = lookup(var.awsprops, "keyname")
+  key_name = var.student
 
   root_block_device {
     volume_size = 8
   }
 
+  # Use the following User Data to install docker and docker compose, clone the repository 
   user_data = <<-EOF
     #!/bin/bash
     set -ex
@@ -119,12 +141,12 @@ resource "aws_instance" "web" {
     sudo usermod -a -G docker ubuntu
     sudo apt install python3-pip -y
     sudo pip3 install docker-compose
-    # sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    # sudo chmod +x /usr/local/bin/docker-compose
-    git clone https://github.com/pzfreo/clo-tf-docker.git
-    cd clo-tf-docker
+    cd /home/ubuntu
+    git clone ${lookup(var.awsprops, "dc-repository")} dc
+    cd dc
     docker-compose up --build
   EOF
+
 
   vpc_security_group_ids = [
     module.ec2_sg.security_group_id,
